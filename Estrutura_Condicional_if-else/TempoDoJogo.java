e587056109c61dec526b6c998b7d0984ef38a7eb
LookAEREo <lucariogamr4@gmail.com>
2025-04-24 19:07:30 -0300

    Add files via upload

---
+++ Estrutura_Condicional_if-else/TempoDoJogo.java
@@ -0,0 +1,31 @@
+package bob_mimi;
+
+import java.util.Scanner;
+
+public class TempoDoJogo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int IN, FIM;
+		int HORARIO;
+		
+		System.out.println("Digite o horario de inicio do jogo:");
+		IN = sc.nextInt();
+		
+		System.out.println("Digite o horario de termino do jogo");
+		FIM = sc.nextInt();
+		
+		if (IN < FIM) {
+			HORARIO  = FIM - IN; 
+		} else {
+			HORARIO = 24 - IN + FIM;
+		}
+		
+	    System.out.println("O JOGO DUROU " + HORARIO + " HORA(S)");
+		
+		sc.close();
+	}
+
+}
